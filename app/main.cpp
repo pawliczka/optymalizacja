5e5a881cd82ce6cad7b44482ac9ad11a9c8a7049
Pawel Baran <pawel6x30@gmail.com>
2017-03-21 20:00:20 +0100

    added boost support

--- app/main.cpp
+++ app/main.cpp
@@ -2,11 +2,15 @@
 #include <QWidget>
 #include <iostream>
 #include "mainwindow.h"
+#include <Eigen/Dense>
+#include <boost/math/tools/polynomial.hpp>
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     mainWindow w;
     w.show();
-    return a.exec();
+    boost::math::tools::polynomial<double> const b{{-2.0, 1.0}};
+    std::cout << b;
+   // return a.exec();
 }
--- defaults.pri
+++ defaults.pri
@@ -1,5 +1,7 @@
 INCLUDEPATH += $$PWD/src
+INCLUDEPATH += $$PWD/tests/mocks/
+INCLUDEPATH += /usr/local/include/eigen3
+INCLUDEPATH += /usr/local/include
 SRC_DIR = $$PWD
 
 QMAKE_CXXFLAGS += -std=c++14 -Wall -Wextra -pedantic
-INCLUDEPATH += /usr/local/include/eigen3
