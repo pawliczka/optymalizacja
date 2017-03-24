705c4f8321cff26af270c96a69f62d7c85b45c2c
Pawel Baran <pawel6x30@gmail.com>
2017-03-24 23:07:16 +0100

    SingleExpresionConventer ut refactor

--- tests/SingleExpresionConventerTestSuite.cpp
+++ tests/SingleExpresionConventerTestSuite.cpp
@@ -10,56 +10,48 @@ public:
 
 TEST_F( SingleExpresionConventerTestSuite, shouldReturnZeroZeroPairWhenReceiveEmptyString )
 {
-    std::string expresion = "";
-    auto result = sut.convert( expresion );
+    auto result = sut.convert( "" );
     ASSERT_EQ( result, std::make_pair( 0, 0 ) );
 }
 
 TEST_F( SingleExpresionConventerTestSuite, shouldReturnZeroZeroPairWhenReceiveEmptyStringwithSpaces )
 {
-    std::string expresion = "   ";
-    auto result = sut.convert( expresion );
+    auto result = sut.convert( "   " );
     ASSERT_EQ( result, std::make_pair( 0, 0 ) );
 }
 
 TEST_F( SingleExpresionConventerTestSuite, shouldReturnOneZeroPairWhenReceive1String )
 {
-    std::string expresion = "1";
-    auto result = sut.convert( expresion );
+    auto result = sut.convert( "1" );
     ASSERT_EQ( result, std::make_pair( 1, 0 ) );
 }
 
 TEST_F( SingleExpresionConventerTestSuite, shouldReturnOneZeroPairWhenReceive1StringWithSpace )
 {
-    std::string expresion = " 1";
-    auto result = sut.convert( expresion );
+    auto result = sut.convert( " 1" );
     ASSERT_EQ( result, std::make_pair( 1, 0 ) );
 }
 
 TEST_F( SingleExpresionConventerTestSuite, shouldReturnOneOnePairWhenReceiveX1StringWithSpace )
 {
-    std::string expresion = " x1";
-    auto result = sut.convert( expresion );
+    auto result = sut.convert( " x1" );
     ASSERT_EQ( result, std::make_pair( 1, 1 ) );
 }
 
 TEST_F( SingleExpresionConventerTestSuite, shouldReturnThreeTwoPairWhenReceive3X2String )
 {
-    std::string expresion = " 3x2";
-    auto result = sut.convert( expresion );
+    auto result = sut.convert( " 3x2" );
     ASSERT_EQ( result, std::make_pair( 3, 2 ) );
 }
 
 TEST_F( SingleExpresionConventerTestSuite, shouldReturnMinusThreeTenPairWhenReceiveMinus3x10String )
 {
-    std::string expresion = " -3x10";
-    auto result = sut.convert( expresion );
+    auto result = sut.convert( " -3x10" );
     ASSERT_EQ( result, std::make_pair( -3, 10 ) );
 }
 
 TEST_F( SingleExpresionConventerTestSuite, shouldReturnMinusFiftyThreeTenPairWhenReceiveMinus53x10String )
 {
-    std::string expresion = " -53x10 ";
-    auto result = sut.convert( expresion );
+    auto result = sut.convert( " -53x10 " );
     ASSERT_EQ( result, std::make_pair( -53, 10 ) );
 }
