8ca01724c995f469e9cba0cff073261cac24249e
Pawel Baran <pawel.baran@nokia.com>
2017-03-27 12:00:59 +0200

    TermConventer Refactor

--- src/TermConverter.cpp
+++ src/TermConverter.cpp
@@ -1,51 +1,53 @@
+#include "TermConverter.hpp"
 #include <algorithm>
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
-#include "TermConverter.hpp"
 
-Term TermConverter::convert( std::string expresion )
+namespace
 {
-    m_expresionWithoutWhiteCharacters = removeWhiteCharacters( expresion );
-    if ( m_expresionWithoutWhiteCharacters.empty() )
-        return Term( 0, 0 );
-    return splitExpresionToPair();
+std::string & removeWhiteCharacters(std::string & expresion)
+{
+    expresion.erase(std::remove(expresion.begin(), expresion.end(), ' '), expresion.end());
+    return expresion;
+}
+bool isExpresionWithoutCoefficient(const std::vector<std::string> & splitedExpresion)
+{
+    return splitedExpresion[0].empty();
 }
 
-std::string& TermConverter::removeWhiteCharacters( std::string& expresion )
+bool isTermWithVariable(const std::vector<std::string> & splitedExpresion)
 {
-    expresion.erase( std::remove( expresion.begin(), expresion.end(), ' ' ), expresion.end() );
-    return expresion;
+    return splitedExpresion.size() == 2;
+}
+}
+
+Term TermConverter::convert(std::string expresion)
+{
+    m_expresionWithoutWhiteCharacters = removeWhiteCharacters(expresion);
+    if(m_expresionWithoutWhiteCharacters.empty())
+        return Term(0, 0);
+    splitExpresionByX();
+    return m_splitedExpresion;
 }
 
 void TermConverter::splitExpresionByX()
 {
-    boost::split( m_splitedExpresion, m_expresionWithoutWhiteCharacters, boost::is_any_of( "x" ) );
-}
-
-Term TermConverter::splitExpresionToPair()
-{
-    splitExpresionByX();
-    if ( isFullyExpresion() )
-        return convertFullyExpresion();
+    std::vector<std::string> splitedExpresion;
+    boost::split(splitedExpresion, m_expresionWithoutWhiteCharacters, boost::is_any_of("x"));
+    if(isTermWithVariable(splitedExpresion))
+    {
+        if(isExpresionWithoutCoefficient(splitedExpresion))
+        {
+            m_splitedExpresion = {1, boost::lexical_cast<int>(splitedExpresion[1])};
+        }
+        else
+        {
+            m_splitedExpresion = {boost::lexical_cast<int>(splitedExpresion[0]),
+                                  boost::lexical_cast<int>(splitedExpresion[1])};
+        }
+    }
     else
-        return Term( boost::lexical_cast< int >( m_splitedExpresion[ 0 ] ), 0 );
-}
-
-Term TermConverter::convertFullyExpresion()
-{
-    if ( isExpresionWithoutCoefficient() )
-        return Term( 1, boost::lexical_cast< int >( m_splitedExpresion[ 1 ] ) );
-    else
-        return Term( boost::lexical_cast< int >( m_splitedExpresion[ 0 ] ),
-            boost::lexical_cast< int >( m_splitedExpresion[ 1 ] ) );
-}
-
-bool TermConverter::isExpresionWithoutCoefficient()
-{
-    return m_splitedExpresion[ 0 ] == "";
-}
-
-bool TermConverter::isFullyExpresion()
-{
-    return m_splitedExpresion.size() == 2;
+    {
+        m_splitedExpresion = {boost::lexical_cast<int>(splitedExpresion[0]), 0};
+    }
 }
--- src/TermConverter.hpp
+++ src/TermConverter.hpp
@@ -9,13 +9,7 @@ public:
 
 private:
     std::string m_expresionWithoutWhiteCharacters;
-    std::vector< std::string > m_splitedExpresion;
+    Term m_splitedExpresion;
 
-    std::string& removeWhiteCharacters( std::string& expresion );
-    Term splitExpresionToPair();
-    Term convertFullyExpresion();
-    Term convertWithoutVariable( const std::vector< std::string >& splitedExpresion );
     void splitExpresionByX();
-    bool isExpresionWithoutCoefficient();
-    bool isFullyExpresion();
 };
