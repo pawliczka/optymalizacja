b385a2406cfa4f8d2cb9b86859193f950a77c409
Mateusz Malon <mateusz.malon14@gmail.com>
2017-03-31 09:06:11 +0200

    Added functions to Equation class

--- src/Equation.cpp
+++ src/Equation.cpp
@@ -5,3 +5,23 @@ Equation::Equation()
 
 }
 
+int Equation::getCoefficient(int index)
+{
+
+}
+
+ComparisonOperator Equation::getComparisonOperator()
+{
+
+}
+
+void Equation::setCoefficient(int value, int index)
+{
+
+}
+
+void Equation::setComparisonOperator(ComparisonOperator comparisonOperator)
+{
+
+}
+
--- src/Equation.hpp
+++ src/Equation.hpp
@@ -6,6 +6,10 @@ class Equation
 {
 public:
     Equation();
+    int getCoefficient(int index);
+    ComparisonOperator getComparisonOperator();
+    void setCoefficient(int value, int index);
+    void setComparisonOperator(ComparisonOperator comparisonOperator);
 private:
     std::vector<int> m_coefficients;
     ComparisonOperator m_comparisonOperator;
